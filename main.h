22c4fddf0549b7f5080c7b6d5665c95d2ab1ddf3
Robert Santana <123roabel@gmail.com>
2023-12-14 01:26:28 -0400

    fix main

--- main.h
+++ main.h
@@ -1,3 +1,6 @@
+
+#ifndef main_h
+#define main_h
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -5,6 +8,8 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
-void display_prompt()
-void execute_command(const char *command)
-int main(void)
+void display_prompt();
+void execute_command(const char *command);
+int main(void);
+
+#endif
