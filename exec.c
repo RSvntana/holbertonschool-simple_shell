348811753110601385b841de2d63265b93a07559
Pedro Augusto Laguna <lagunaaustore@gmail.com>
2023-12-16 21:16:11 -0400

    Adding parse function and debugging prompt

--- exec.c
+++ exec.c
@@ -5,48 +5,50 @@
  * Return: Success (0)
 */
 
-int display_prompt(void)
+void display_prompt(void)
 {
-	char *input = NULL;
-	size_t input_size = 0;
+    char user_input[100];  // Assuming input won't exceed 99 characters
 
-	while (1)
-	{
-		printf("Simple Shell $ "); /* Shell display */
+    while (1) {
+        printf("Simple Shell $ ");
+        fgets(user_input, sizeof(user_input), stdin);
+        user_input[strcspn(user_input, "\n")] = '\0';  // Remove trailing newline character
 
-		if (getline(&input, &input_size, stdin) == -1)
-		{
-			printf("Error reading input, Exit Shell.\n");
-			break; /* Handles error or end */
-		}
+        if (strcmp(user_input, "exit") == 0 || strcmp(user_input, "quit") == 0) {
+            printf("Exiting the program... Goodbye!\n"); // Exit or quit program options
+            break;
+    }
+}
+}
+void parse_input(const char *input, ParsedInput *parsed) 
+{
+   
+    strcpy(parsed->command, "");
+    parsed->num_args = 0; // Initialize the structure
 
-		input[strcspn(input, "\n")] = '\0'; /* Remove newline char from input */
+    
+    char *token = strtok((char *)input, " ");// Tokenize the input based on spaces
+    
+    if (token != NULL) {
+        strcpy(parsed->command, token);// Extract the comman
+    }
 
-		if (strcmp(input, "exit") == 0)
-		{
-			break; /* Checks if user wishes to exit */
-		}
 
-		printf("ENTERED: %s\n", input); /* This echoes the input */
-	}
-
-	free(input); /* Free dynamically allocated memory */
-	return (0);
+    while (token != NULL) {
+        token = strtok(NULL, " ");
+        if (token != NULL) {
+            strcpy(parsed->arguments[parsed->num_args], token);
+            parsed->num_args++;
+        }// Extract the arguments
+    }
 }
 
-/**
- * main - execve example
- *
- * Return: Always 0.
- */
-int main(void)
-{
+
+ int main()
+ {
+	 char user_input[MAX_INPUT_LENGTH];
+    ParsedInput parsed;
 	display_prompt();
-
-	char *argv[] = {"/bin/", NULL};/* Example command and arguments */
-
-	if (execve(argv[0], argv, NULL) == -1)
-	{
-		return (0);
-	}
-}
+	parse_input(user_input, &parsed);
+	return (0);
+ }
