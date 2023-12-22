dbeb91ea2bfeab332aac07bcf9f8cafa1b050467
Pedro Augusto Laguna <lagunaaustore@gmail.com>
2023-12-22 01:26:31 -0400

    Betty corrrection in the simple_shell.c file

--- simple_shell.c
+++ simple_shell.c
@@ -1,9 +1,9 @@
 #include "shell.h"
 /**
- * get_path - Get the full path of a command
- * @comd_name: The name of the command
+ * get_path - Get full path of a command
+ * @comd_name: command name
  * Return: A dynamically allocated string containing the full path
- *         NULL if the command is not found
+ *         or NULL if the command not found
  */
 char *get_path(char *comd_name)
 {
@@ -13,6 +13,8 @@ char *get_path(char *comd_name)
 	if (access(comd_name, X_OK) == 0)
 	{
 		conpath = strdup(comd_name);
+
+
 		if (conpath == NULL)
 		{
 			perror("malloc");
@@ -20,14 +22,21 @@ char *get_path(char *comd_name)
 		}
 		return (conpath);
 	}
+
+
 	conpath = malloc(strlen(comd_path) + strlen(comd_name) + 1);
+
+
 	if (conpath == NULL)
 	{
 		perror("malloc");
 		free(conpath);
 		return (NULL);
 	}
+
 	sprintf(conpath, "%s%s", comd_path, comd_name);
+
+
 	if (access(conpath, X_OK) != 0)
 	{
 		perror("Command not found");
@@ -36,6 +45,8 @@ char *get_path(char *comd_name)
 	}
 	return (conpath);
 }
+
+
 /**
  * Tokenize - Tokenize a command string and execute it
  * @token: The command string to tokenize
@@ -51,7 +62,6 @@ void Tokenize(char *token, char **env)
 	tok = strtok(token, " \n"); /* Tokenize the command string */
 	if (tok == NULL)
 		return;
-
 	while (tok != NULL)
 	{
 		tokens = realloc(tokens, sizeof(char *) * (index + 1));
@@ -61,8 +71,8 @@ void Tokenize(char *token, char **env)
 			return;
 		}
 		tokens[index] = strdup(tok); /* Store token in tokens array */
-		index++;					   /* Increment the token count */
-		tok = strtok(NULL, " \n");	   /* Get next token from command string */
+		index++;					   /* Index */
+		tok = strtok(NULL, " \n");	   /* Get the next token on command string */
 	}
 	tokens = realloc(tokens, sizeof(char *) * (index + 1));
 	if (tokens == NULL)
@@ -84,7 +94,11 @@ void Tokenize(char *token, char **env)
 	free_token(tokens, index);
 }
 
-
+/**
+ * exec_com - Execute a command
+ * @comd: The command to execute
+ * @env: The environment variables
+ */
 
 void exec_com(char *comd, char **env)
 {
@@ -96,10 +110,6 @@ void exec_com(char *comd, char **env)
 	child_make(&all_path, env);
 	free(all_path);
 }
-
-
-
-
 
 
 /**
@@ -115,7 +125,6 @@ void child_make(char **tokens, char **env)
 	if (pid == -1)
 	{
 		perror("fork"); /* Print error message if fork fails */
-		free_token(tokens, 0);
 		return;
 	}
 	if (pid == 0)
@@ -126,7 +135,7 @@ void child_make(char **tokens, char **env)
 	}
 	else
 	{
-		/* Wait specifically for the child process to complete */
-		waitpid(pid, &status, 2);
+		/* Wait for the child process to finish */
+		waitpid(pid, &status, 0);
 	}
 }
